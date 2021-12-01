26019141192ad19b27abfd85be641fc2cd03247c
Morgan Carroll <modcarroll@gmail.com>
2021-12-01 14:05:45 -0600

    Update piiscrub.js

--- piiscrub.js
+++ piiscrub.js
@@ -2,35 +2,35 @@
 
 function presendHandler(obj) {
   var userInput = obj.data.input.text.toLowerCase()
-
-  // e-mail address regular expression
-  emailReg = "[a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6}"
-
-  // social security number w/ dashes regular expression
-  ssndReg = "^\d{3}-?\d{2}-?\d{4}$"
-
-  // social security number regular expression
-  ssnReg = "^\d{9}$"
-
-  // phone number regular expression
-  phoneReg = "^([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4}|[a-zA-Z0-9]{7})$"
   
-  // m(m)/d(d)/y(y)
-  dobSlash = "^\d{1,2}\/\d{1,2}\/\d{2,4}$"
+ // e-mail address regular expression
+ emailReg = /([a-zA-Z0-9_\.-]+)@([\da-zA-Z\.-]+)\.([a-zA-Z\.]{2,6})/g
 
-  // m(m)-d(d)-y(y)
-  dobDash = "^\d{1,2}\-\d{1,2}\-\d{2,4}$"
+ // social security number w/ dashes regular expression
+ ssndReg = /[0-9]{3}\-?[0-9]{2}\-?[0-9]{4}/g
 
-  // MM(MM) d(d) y(y) 
-  dobLong = "(Jan(?:uary)?|Feb(?:ruary)?|Mar(?:ch)?|Apr(?:il)?|May|Jun(?:e)?|Jul(?:y)?|Aug(?:ust)?|Sep(?:tember)?|Oct(?:ober)?|Nov(?:ember)?|Dec(?:ember)?)\s+(\d{1,2}),?\s+(\d{0,4})"
+ // social security number regular expression
+ ssnReg = /(\d{9})/g
 
-  // m(m) d(d) y(y)
-  dobDigitswSpace = "(\d{1,2})?\s+(\d{1,2})\s+(\d{2,4})"
+ // phone number regular expression
+ phoneReg = /([0-9]( |-)?)?(\(?[0-9]{3}\)?|[0-9]{3})( |-)?([0-9]{3}( |-)?[0-9]{4})/g
+ 
+ // m(m)/d(d)/y(y)
+ dobSlash = /\d{1,2}\/\d{1,2}\/\d{2,4}/g
 
-  // Any number between 6 and 8 digits. 
-  dobDigitsOnly = "^[0-9]{6,8}$"
-  
-  myNameIs = "My\sname\sis\s.*"
+ // m(m)-d(d)-y(y)
+ dobDash = /\d{1,2}\-\d{1,2}\-\d{2,4}/g
+
+ // MM(MM) d(d) y(y) 
+ dobLong = /(jan(?:uary)?|feb(?:ruary)?|mar(?:ch)?|apr(?:il)?|may|jun(?:e)?|jul(?:y)?|aug(?:ust)?|sep(?:tember)?|oct(?:ober)?|nov(?:ember)?|dec(?:ember)?)\s+(\d{1,2}),?\s+(\d{0,4})/g
+
+ // m(m) d(d) y(y)
+ dobDigitswSpace = /(\d{1,2})?\s+(\d{1,2})\s+(\d{2,4})/g
+
+ // Any number between 6 and 8 digits. 
+ dobDigitsOnly = /[0-9]{6,8}/g
+ 
+ myNameIs = /my\sname\sis\s.*/g
 
   userInput = userInput.replace(emailReg, 'REDACTED').replace(ssndReg, 'REDACTED').replace(ssnReg, 'REDACTED').replace(phoneReg, 'REDACTED').replace(dobSlash,'REDACTED').replace(dobDash,'REDACTED').replace(dobLong,'REDACTED').replace(dobDigitswSpace,'REDACTED').replace(dobDigitsOnly,'REDACTED').replace(myNameIs,'REDACTED')
   obj.data.input.text = userInput
